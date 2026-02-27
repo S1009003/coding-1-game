810621e4221ac14c1efa5d3ea8ccaf127f2d7080
S1009003 <S1009003@student.knoxschools.org>
2026-02-27 14:52:57 +0000

    1234

--- student_work/game.py
+++ student_work/game.py
@@ -3,7 +3,7 @@ import curses
 game_data = {
     # Store board dimensions, player/enemy positions, score, energy, collectibles, and icons
 }
-
+(67)
 def draw_board(screen):
     # Print the board and all game elements using curses
 
