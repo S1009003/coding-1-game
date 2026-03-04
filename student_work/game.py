ca5c8bed6ecbc0d185b51a8ccb94b06673f712e4
s960275 <s960275@student.knoxschools.org>
2026-03-04 14:34:50 +0000

    Controls

--- student_work/game.py
+++ student_work/game.py
@@ -14,3 +14,23 @@ game_data = {
 # Item to find is a rflx relic 
 def draw_board(screen):
     # Print the board and all game elements using curses
+
+# Controls Here
+
+def move_player(key):
+    x = game_data['player']['x']
+    y = game_data['player']['y']
+
+    new_x, new_y = x, y
+    key = key.lower()
+
+    if key == "w" and y > 0:
+        new_y -= 1
+    elif key == "s" and y < game_data['height'] - 1:
+        new_y += 1
+    elif key == "a" and x > 0:
+        new_x -= 1
+    elif key == "d" and x < game_data['width'] - 1:
+        new_x += 1
+    else:
+        return  # Invalid key or move off board
