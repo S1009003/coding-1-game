2c0c34ea423440b9df7c1514638ac670d1a8a6da
s960275 <s960275@student.knoxschools.org>
2026-03-04 15:09:09 +0000

    Finished phase II

--- student_work/game.py
+++ student_work/game.py
@@ -12,7 +12,36 @@ game_data = {
     #store curses here
 }
 # Item to find is a bag of gold 
-def draw_board(screen):
+def draw_board(stdscr):
+    curses.start_color()
+    curses.use_default_colors()
+    curses.init_pair(1, curses.COLOR_WHITE, -1)
+
+    stdscr.clear()
+    for y in range(game_data['height']):
+        row = ""
+        for x in range(game_data['width']):
+            # Player
+            if x == game_data['player']['x'] and y == game_data['player']['y']:
+                row += game_data['turtle']
+            # Eagle
+            elif x == game_data['eagle_pos']['x'] and y == game_data['eagle_pos']['y']:
+                row += game_data['eagle_icon']
+            # Obstacles
+            elif any(o['x'] == x and o['y'] == y for o in game_data['obstacles']):
+                row += game_data['obstacle']
+            # Collectibles
+            elif any(c['x'] == x and c['y'] == y and not c['collected'] for c in game_data['collectibles']):
+                row += game_data['leaf']
+            else:
+                row += game_data['empty']
+        stdscr.addstr(y, 0, row, curses.color_pair(1))
+
+    stdscr.refresh()
+    stdscr.getkey()  # pause so player can see board
+
+curses.wrapper(draw_board)
+
     # Print the board and all game elements using curses
 
 # Controls Here
