8bcf3bcb318fb634c1c3aa2c69e46afda2a1c7a3
s960275 <s960275@student.knoxschools.org>
2026-05-12 05:15:50 +0000

    Game

--- student_work/game.py
+++ student_work/game.py
@@ -1,75 +1,78 @@
 # Write your game here
 import curses
-import time
-import random
+
 game_data = {
     'width': 10,
     'height': 10,
     'player': {"x": 0, "y": 0, "score": 0},
-     #icons
-    'MENS_SYMBOL': {'icon': "\U0001F6B9", 'x': 0, 'y': 0},
-    'BLACK_MEDIUM_SQUARE': {'icon': "\U000025FC", 'x': 3, 'y': 3},
-    'BLACK_UP-POINTING_DOUBLE_TRIANGLE' : {'icon': "\U000023EB", 'x': 1, 'y': 1},
-    'MONEY_BAG' : {'icon': "\U0001F4B0", 'x': 2, 'y': 2}, #💰
-    'empty' : "  ",
-    'collected' : []
-    #store curses here
+    'MENS_SYMBOL': {'icon': "P "},
+    'MONEY_BAG': {'icon': "$ ", 'x': 5, 'y': 5},
+    'OBSTACLE': {'icon': "X ", 'x': 3, 'y': 3},
+    'UP_TRIANGLE': {'icon': "^ ", 'x': 1, 'y': 1},
+    'empty': ". ",
+    'game_over': False
 }
-# Item to find is a bag of gold
+
 def draw_board(stdscr):
-    curses.start_color()
-    curses.use_default_colors()
-    curses.init_pair(1, curses.COLOR_WHITE, -1)
+    curses.curs_set(0)
+    stdscr.nodelay(False)
 
-    stdscr.clear()
-    for y in range(game_data['height']):
-        row = ""
-        for x in range(game_data['width']):
-            # Player
-            if x == game_data['player']['x'] and y == game_data['player']['y']:
-                row += game_data['MENS_SYMBOL']['icon']
-            # Triangle
-            elif x == game_data['BLACK_UP-POINTING_DOUBLE_TRIANGLE']['x'] and y == game_data['BLACK_UP-POINTING_DOUBLE_TRIANGLE']['y']:
-                 row += game_data['BLACK_UP-POINTING_DOUBLE_TRIANGLE']['icon']
-            # Obstacles
-            elif x == game_data['BLACK_MEDIUM_SQUARE']['x'] and y == game_data['BLACK_MEDIUM_SQUARE']['y']:
-                 row += game_data['BLACK_MEDIUM_SQUARE']['icon']
-            #any(o['x'] == x and o['y'] == y for o in game_data['BLACK_MEDIUM_SQUARE']):
-                 row += game_data['BLACK_MEDIUM_SQUARE']['icon']
-            # Collectibles
-            elif x == game_data['MONEY_BAG']['x'] and y == game_data['MONEY_BAG']['y']:
-                 row += game_data['MONEY_BAG']['icon']
-            else:
-                row += game_data['empty']
-        stdscr.addstr(y, 0, row, curses.color_pair(1))
+    while not game_data['game_over']:
+        stdscr.clear()
+        stdscr.addstr(0, 0, f"Score: {game_data['player']['score']} | WASD to move, Q to quit")
 
-    stdscr.refresh()
-    stdscr.getkey()  # pause so player can see board
+        for y in range(game_data['height']):
+            for x in range(game_data['width']):
+                char = game_data['empty']
+                if x == game_data['player']['x'] and y == game_data['player']['y']:
+                    char = game_data['MENS_SYMBOL']['icon']
+                elif x == game_data['MONEY_BAG']['x'] and y == game_data['MONEY_BAG']['y']:
+                    char = game_data['MONEY_BAG']['icon']
+                elif x == game_data['OBSTACLE']['x'] and y == game_data['OBSTACLE']['y']:
+                    char = game_data['OBSTACLE']['icon']
+                elif x == game_data['UP_TRIANGLE']['x'] and y == game_data['UP_TRIANGLE']['y']:
+                    char = game_data['UP_TRIANGLE']['icon']
+                
+                stdscr.addstr(y + 1, x * 2, char)
 
-curses.wrapper(draw_board)
+        stdscr.refresh()
+        key = stdscr.getch()
+        
+        try:
+            key_char = chr(key).lower()
+        except:
+            key_char = ""
 
-    # Print the board and all game elements using curses
-
-# Controls Here
+        if key_char == 'q':
+            break
+        
+        move_player(key_char)
 
 def move_player(key):
     x = game_data['player']['x']
     y = game_data['player']['y']
-
     new_x, new_y = x, y
-    key = key.lower()
 
-    if key == "w" and y > game_data['height'] -1:
-        new_y += 1
-    elif key == "s" and y < game_data['height'] +1:
+    if key == "w" and y > 0:
         new_y -= 1
-    elif key == "a" and x > game_data['width'] +1:
-        new_x += 1
-    elif key == "d" and x < game_data['width'] -1:
+    elif key == "s" and y < game_data['height'] - 1:
+        new_y += 1
+    elif key == "a" and x > 0:
         new_x -= 1
+    elif key == "d" and x < game_data['width'] - 1:
+        new_x += 1
     else:
         return  
 
-    if any(o['x'] == new_x and o['y'] == new_y for o in game_data['obstacles']):
+    if new_x == game_data['OBSTACLE']['x'] and new_y == game_data['OBSTACLE']['y']:
         return
-   
+
+    if new_x == game_data['MONEY_BAG']['x'] and new_y == game_data['MONEY_BAG']['y']:
+        game_data['player']['score'] += 10
+        game_data['MONEY_BAG']['x'], game_data['MONEY_BAG']['y'] = -1, -1 
+
+    game_data['player']['x'] = new_x
+    game_data['player']['y'] = new_y
+
+if __name__ == "__main__":
+    curses.wrapper(draw_board)
